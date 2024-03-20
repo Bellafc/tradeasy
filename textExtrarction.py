144934d822bdbc7bd38154898d454688855247f6
Zheng Chen <94959679+Bellafc@users.noreply.github.com>
2024-03-20 18:22:40 +0800

    Update textExtrarction.py

--- textExtrarction.py
+++ textExtrarction.py
@@ -1,3 +1,102 @@
+##-----------------------  GET STANDARD LIST --------------------------------------------------------------------------------------------------------------------------##
+####brand
+def get_brand_dict(brand_df):
+    brand_df = brand_df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+    brand_df = brand_df.T
+    new_columns = brand_df.iloc[0]
+    df_new = brand_df[1:].reset_index(drop=True)
+    df_new.columns = new_columns
+    result_dict = df_new.to_dict('list')
+    brand_dict = {k: [i for i in v if type(i) != float] for k, v in result_dict.items()}
+    brand_dict['ibp'].append("i. b. p.")
+    brand_dict['ibp'].append("i. b. p")
+    brand_dict['frimesa'].append("frimsa")
+    brand_dict['fortefrigo'] = None
+    brand_dict['lkj'] = None
+    brand_dict['nossobeef'] = None
+    brand_dict['national'] = None
+    brand_dict['swift'] = None
+    brand_dict['kiwi'] = None
+    return brand_dict
+
+def get_brand_list(brand_dict):
+    brand_list = []
+    for key, values in brand_dict.items():
+        brand_list.append(key)
+        try:
+            brand_list.extend(values)
+        except:
+            print(key)
+    return brand_list
+
+##origin
+def get_origin_list():
+    return ['西', '巴', '德', '丹', '波', '澳', '中', '匈牙利', '德', '俄', '愛', '墨', '荷', '阿', '荷 蘭', '挪 威', '英 國', '英', '紐', '法 國', '爱', '加', '美', '德國']
+
+##product
+def get_product_dict(product_df):
+    df_clean = product_df.dropna(axis=1, how='all')
+    df_melted = df_clean.melt(id_vars=['category'], value_name='product').dropna(subset=['product']).drop('variable', axis=1)
+    df_grouped = df_melted.groupby('category')['product'].apply(list).reset_index()
+    product_dict = df_grouped.set_index('category')['product'].to_dict()
+    product_dict['Pork'].pop()
+    product_dict['Pork'].remove('筒骨')
+    product_list = []
+    for key, values in product_dict.items():
+        for value in values:
+            product_list.append(value)
+    value = product_dict.pop('Meatball')
+    product_dict['Meatball'] = value
+    product_dict['Beef'].append('牛')
+    product_dict['Chicken'].append('雞')
+    product_dict['Chicken'].append('鸡')
+    product_dict['Lamb'].append('羊')
+    product_dict['Pork'].append('豬')
+    product_dict['Poultry'].append('鴨')
+    return product_dict, product_list
+
+##grade
+def get_grade_dict(grade_df):
+    grade_df = grade_df.T
+    grade_df = grade_df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+    new_columns = grade_df.iloc[0]
+    df_new = grade_df[1:].reset_index(drop=True)
+    df_new.columns = new_columns
+    result_dict = df_new.to_dict('list')
+    grade_dict = {k: [i for i in v if type(i) != float] for k, v in result_dict.items()}
+    del grade_dict['m']
+    del grade_dict['s']
+    del grade_dict['l']
+    del grade_dict['a']
+    grade_list = []
+    for key, values in grade_dict.items():
+        grade_list.append(key)
+        grade_list.extend(values)
+    return grade_dict, grade_list
+
+## Country
+def get_country_dict(country_df):
+    country_df = country_df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+    country_df = country_df.T
+    new_columns = country_df.iloc[0]
+    df_new = country_df[1:].reset_index(drop=True)
+    df_new.columns = new_columns
+    result_dict = df_new.to_dict('list')
+    country_dict = {k: [i for i in v if type(i) != float] for k, v in result_dict.items()}
+    country_dict.pop('standard_country')
+    country_list = []
+    for key, values in country_dict.items():
+        country_list.append(key)
+        try:
+            country_list.extend(values)
+        except:
+            print(key)
+    return country_dict, country_list
+
+
+##-------------------------------------------  GET COLUMNS --------------------------------------------------------------------------------------------------------------------------##
+
+
 def get_origin(row, origin_list):
     for origin_item in origin_list:
         if origin_item in row['貨名']:
